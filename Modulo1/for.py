44d5a4c12a2f86638dd0194d81615caa9772a917
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-07-28 08:42:31 -0300

    Create for.py
    tipos de for

---
+++ Modulo1/for.py
@@ -0,0 +1,24 @@
+nome="Alexandre"
+lista = [1,3,5,7,9]
+
+#for letra in nome:
+#   print(letra)
+
+#for num in lista:
+#    print(num)
+
+#for num in range(8,14):
+#    print(num)
+
+#for c in nome:
+#    print(nome[c])
+
+#for num in range(11): #começa em 0 e vai ate a sentença de parada
+#    print(num)
+
+#for num in range(0,11,2): #ira do ponto inicial ate o ponto de parada
+                          #pulando de 2 em 2  
+#    print(num)
+
+#for num in range(15,0,-3): #do ponto inicial ao final com decremento
+#    print(num)
