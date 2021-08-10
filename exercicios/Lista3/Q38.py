e81b96743b9808f909afe0b40c49e75c21547bc3
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-08-10 20:57:27 -0300

    Update Q38.py

---
+++ exercicios/Lista3/Q38.py
@@ -0,0 +1,7 @@
+#Faça um programa que calcule o terno pitagórico a, b, c, 
+#para o qual a+b+c = 1000. Um terno pitagórico é um conjunto
+#de três números naturais, a, b, c, para a qual,
+
+#+V=2
+#Por exemplo,
+#F +4P=9+16=25=5
