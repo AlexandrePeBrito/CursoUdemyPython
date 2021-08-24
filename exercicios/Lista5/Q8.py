2cd5f55ae1f793d1830640fe1497a36b4102f884
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-08-24 14:36:45 -0300

    atividade

--- exercicios/Lista5/Q8.py
+++ exercicios/Lista5/Q8.py
@@ -1,3 +1,11 @@
 #8. Sejam a e b os catetos de um triângulo, onde a hipotenusa é obtida pela equação:
-#hipotenusa = Va? +b?. Faça uma função que receba os valores de a e b e calcule o
+#hipotenusa =raiz(a^2+b^2) Faça uma função que receba os valores de a e b e calcule o
 #valor da hipotenusa através da equação.
+import math
+def hip(a,b):
+    return math.sqrt(pow(a,2)+pow(b,2))
+
+a=float(input('Informe o valor do cateto: '))
+b=float(input('Informe o valor do cateto: '))
+h=hip(a,b)
+print(f"O valor da hipotenusa eh {h}")
--- exercicios/Lista5/Q9.py
+++ exercicios/Lista5/Q9.py
@@ -1,3 +1,10 @@
 #9. Façaumam função que receba a altura e o raio de um cilindro circular e retorne o volume
 #do cilindro. O volume de um cilindro circular é calculado por meio da seguinte fórmula:
-#V =n*raio? x altura, onde nx = 3.141592.
+#V =pi*raio^2 x altura, onde pi = 3.141592.
+def volCilindro(raio,altura):
+    return 3.1415926535*pow(raio,2)*altura
+
+r=float(input("Informe o raio do cilindro: "))
+alt=float(input("Informe a altura do cilindro: "))
+volume=volCilindro(3,2)
+print(volume)
