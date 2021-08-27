b89846605ae5c5255852da22f47dd51c5a72b805
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-08-27 17:23:57 -0300

    estudo

---
+++ Modulo3/min_max.py
@@ -0,0 +1,22 @@
+#Max retorna o maior numero de um iteravel ou o maior de 2 ou mais elementos
+#Min retorna o menor numero de um iteravel ou o menor de 2 ou mais elementos
+lista=[9,5,2,1,4,5,3,6,21,5,4,55,0]
+print(max(lista))
+
+print(max(8,9,5,7,4,5,6))
+
+dicionario={'a':0,'b':1,'f':51,'g':12,'q':5,'u':3,'d':2}
+print(max(dicionario))
+print(max(dicionario.values()))
+
+print(min(lista))
+
+print(min(8,9,5,7,4,5,6))
+
+dicionario1={'a':0,'b':1,'f':51,'g':12,'q':5,'u':3,'d':2}
+print(min(dicionario1))
+print(min(dicionario1.values()))
+
+nomes=['alexandre', 'maiure','joselito']
+print(min(nomes))                           #pela ordem alfabetica
+print(max(nomes))                           #pela ordem alfabetica
