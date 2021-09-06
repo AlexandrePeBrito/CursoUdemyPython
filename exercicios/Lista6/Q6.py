b84147922fd4af601b652183087c19ba2fcfbb29
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-09-06 14:02:25 -0300

    Update Q6.py

--- exercicios/Lista6/Q6.py
+++ exercicios/Lista6/Q6.py
@@ -1,3 +1,11 @@
 """ 6. Faça um programa que receba do usuário um arquivo texto e mostre na tela quantas
 vezes cada letra do alfabeto aparece dentro do arquivo.
  """
+arq=input("Informe o caminho do arquivo: ") #CursoUdemyPython/exercicios/Lista6/arq.txt
+arquivo=open(arq)
+texto=arquivo.read()
+a=0
+for c in texto:
+    if(c!=' '):
+        a+=1
+print(a)
