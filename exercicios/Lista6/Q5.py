f5179874413efaa3890e72e1bf1334b198e1d60c
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-09-06 13:56:54 -0300

    Update Q5.py

--- exercicios/Lista6/Q5.py
+++ exercicios/Lista6/Q5.py
@@ -1,3 +1,13 @@
 """ 5. Faça um programa que receba do usuário um arquivo texto e um caracter. Mostre na tela
 quantas vezes aquele caractere ocorre dentro do arquivo.
  """
+arquivo=open('CursoUdemyPython/exercicios/Lista6/arq.txt')
+texto=arquivo.read()
+carac=input('Informe um caractere: ')
+ca=0
+for c in texto:
+    if(c == carac):
+        ca+=1
+arquivo.close()
+
+print(f"Foi identificado {ca} deste caractere")
