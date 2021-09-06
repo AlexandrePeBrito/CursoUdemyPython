63036c60ee7444220e5b8634d70720ea23686a62
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-09-06 15:37:23 -0300

    Update Q11.py

--- exercicios/Lista6/Q11.py
+++ exercicios/Lista6/Q11.py
@@ -1,3 +1,5 @@
 """ 11. Façaum programa no qual o usuário informa o nome do arquivo e uma palavra, e retorne
 o número de vezes que aquela palavra aparece no arquivo.
  """
+arq1=input("Informe o caminho do arquivo: ")#'CursoUdemyPython/exercicios/Lista6/arq.txt'
+palavra=input('Informe uma palavra: ')
