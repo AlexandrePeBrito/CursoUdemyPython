642f044335c3b939ed195a71d9b4723a910d0895
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-09-06 14:05:41 -0300

    Update Q7.py

--- exercicios/Lista6/Q7.py
+++ exercicios/Lista6/Q7.py
@@ -1,2 +1,10 @@
 """ 7. Faça um programa que receba do usuário um arquivo texto. Crie outro arquivo texto
 contendo o texto do arquivo de entrada, mas com as vogais substituídas por ". """
+arquivo=open('CursoUdemyPython/exercicios/Lista6/arq.txt')
+arquivo1=open('CursoUdemyPython/exercicios/Lista6/novo.txt','w') 
+texto=arquivo.read()
+for c in texto:
+    if(c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u'):
+        arquivo1.write('.')
+    else:
+        arquivo1.write(c)
