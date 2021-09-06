8fea052b829d3f46ac441bc20f7917fec0a3b032
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-09-06 14:12:52 -0300

    Update Q8.py

--- exercicios/Lista6/Q8.py
+++ exercicios/Lista6/Q8.py
@@ -1,5 +1,14 @@
 """ 8. Faça um programa que leia o conteúdo de um arquivo e crie um arquivo com o mesmo
 conteúdo, mas com todas as letras minúsculas convertidas para maiúsculas. Os no-
 mes dos arquivos serão fornecidos, via teclado, pelo usuário. A função que converte
-maiúscula para minúscula é o toupper(). Ela é aplicada em cada caractere da string.
- """
+maiúscula para minúscula é o upper(). Ela é aplicada em cada caractere da string.
+"""
+arq='CursoUdemyPython/exercicios/Lista6/arq.txt'
+arquivo=open(arq)
+arquivo2=open('CursoUdemyPython/exercicios/Lista6/novo.txt','w')
+texto=arquivo.read()
+for c in texto:
+    if(c==c.lower()):
+        arquivo2.write(c.upper())
+    else:
+        arquivo2.write(c)
