58a94543bcdfc58eb396a75aef25a7c8a2069cbb
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-09-06 13:15:58 -0300

    Update Q2.py

--- exercicios/Lista6/Q2.py
+++ exercicios/Lista6/Q2.py
@@ -1,3 +1,7 @@
 """ 2. Faça um programa que receba do usuário um arquivo texto e mostre na tela quantas
 linhas esse arquivo possui.
  """
+
+arquivo = open("CursoUdemyPython/exercicios/Lista6/arq.txt")
+linhas=arquivo.readlines()                              #gera uma lista com cada bloco uma linha
+print(len(linhas))
