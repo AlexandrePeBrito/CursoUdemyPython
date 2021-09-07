cb3fd57aaace6918bfeb1996a3c2c9ed93c93e95
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-09-07 20:09:32 -0300

    POO

---
+++ Modulo8/Atributos.py
@@ -0,0 +1,77 @@
+""" 
+Atributos -> Caracteristicas de objetos. Podemos representar os estados de um objeto
+
+Em python dividimos  os atributos em 3 grupos:
+    Atributos de Instancia
+    Atributos de Classe
+    atributos Dinamicos
+
+"""
+#Atributo de Instancia: Atributos declarados dentro do metodo construtor. Sendo obrigatorio
+#todos os objetos terem esses atributos
+
+"""
+#Metodo private
+class Lampada:      
+    def __init__(self, voltagem, cor):              #Metodo Construtor __init__
+        self.__voltagem=voltagem                    #Os 2 __ significa privte
+        self.__cor=cor
+#Chamada:
+lamp=Lampada(120,'branca')
+
+#OBS: Todo atributo de uma classe eh publico(podendo ser acessado por todo o projeto).
+#Para determinar um atributo como privado, utiliza-se o "__"
+
+#Metodo publico
+
+class Usuario:
+    def __init__(self, nome, email, senha):              #Metodo Construtor __init__
+        self.nome=nome                    
+        self.email=email
+        self.__senha=senha
+    def get_senha(self):                                #metodo para mostrar private
+        print(self.__senha)
+#Chamada:
+user=Usuario('alexandre','user@gmail.com', '1243796')
+
+print(user.nome)
+#print(user.__senha)     #ERROR AttributeError
+#print(user._Usuario__senha)     #Temos acesso, mas não deveriamos fazer
+user.get_senha()                 #Modo correto de fazer chamada do private
+"""
+
+
+#Atributos de Classe    -> atributos inicializados ja na classe, fora do construtor
+""" class Lampada:     
+    intensidade='intensa'                           #Atributo de Classe
+    def __init__(self, voltagem, cor):              
+        self.__voltagem=voltagem                    
+        self.__cor=cor
+#Chamada:
+lamp1=Lampada(120,'branca')
+lamp2=Lampada(210,'verde')
+
+print(lamp1.intensidade)                #Acesso Errado
+print(lamp2.intensidade)                #Acesso Errado
+
+print(Lampada.intensidade)              #Acesso Correto """
+
+#Atributo Dinamico  -> Atributo de instancia que pode ser criado em tempo de execução
+#OBS: Atributo Dinamico sera exclusivo da instancia que criou
+#OBS: Pouco usual
+
+class Lampada:                                    
+    def __init__(self, voltagem, cor):              
+        self.__voltagem=voltagem                    
+        self.__cor=cor
+#Chamada:
+lamp1=Lampada(120,'branca')
+lamp2=Lampada(210,'verde')
+
+lamp1.intensidade='Forte'               #Atributo Dinamico 
+print(lamp1.intensidade)                #Atributo Dinamico 
+
+#print(lamp2.intensidade)               #ERROR AttributeError
+
+
+
--- Modulo8/Classes.py
+++ Modulo8/Classes.py
@@ -5,4 +5,17 @@ Em poo  Classes é nada mais que um modelo dos objetos do mundo real  sendo repr
 computacionalmente
 
 Imagine que voce queira fazer  um sistema  para automatizar o controle de lampadas da sua casa
+
+Classes podem conter:
+    *Atributos  -> Caracteristicas do objeto
+    *Metodos    -> Ações/comportamentos do objeto
+
+*Para criar uma classe em python utilizamos a palavra class.
+*Criamos uma classe com a letra inicial maiuscula. Caso nome composto as duas inicias sao maiuculas
+e juntas 
 """
+
+class Lampada:
+    pass                
+
+lamp=Lampada()
