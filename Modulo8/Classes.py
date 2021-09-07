e9164e01bd72695ea78b3b1cbb0bd6524d52e4ac
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-09-07 18:42:39 -0300

    POO

---
+++ Modulo8/Classes.py
@@ -0,0 +1,8 @@
+""" 
+POO - Classes
+
+Em poo  Classes é nada mais que um modelo dos objetos do mundo real  sendo representados 
+computacionalmente
+
+Imagine que voce queira fazer  um sistema  para automatizar o controle de lampadas da sua casa
+"""
---
+++ Modulo8/POO.py
@@ -0,0 +1,20 @@
+""" 
+POO -> É um paradigma da programação que utiliza de mapeamento de objeto do mundo
+real para  modelos computaveis.
+
+-Paradigma é uma forma/metodologia utilizada para pensar/desenvolver sistemas
+
+-Principais elementos de POO:
+    *Classe     -> Modelo de um objeto real sendo representado computacionalmente
+    *Atributo   -> Caracteristicas do objeto
+    *Metodo     -> Comportamento do objeto (igual as funções)
+    *Construtor -> Metodo especial para criar objetos
+    *Objeto     -> Instancia da classe
+"""
+
+class produto:              #Criando classe
+    pass
+
+ps4=produto()               #Instaciando objeto
+print(ps4)
+print(type(ps4))
